4116694710ebd0c6ed0b233390d2e163e61370ea
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2025-07-16 10:05:10 -0300

    Update page.js

--- Receita 14(app/page.js
+++ Receita 14(app/page.js
@@ -1,42 +1,30 @@
-  <div> 
-           <div> 
-              <h1>Viva Santana!</h1>  
-              <Link href="/novarota">Rota1</Link> <br/><br/>
-              <a href="/novarota">Rota 1, jeito antigo</a>
-           </div>
-       </div>
-   
+ 
+import Link from 'next/link'
 
-        <html lang="en">
-            <body>
-                <header>
-                    <nav>
-                        <Link href="/">Home</Link> | <Link href="/rota2">Rota 2</Link> | <Link href="/rota3">Rota 3</Link>
-                    </nav>
-                </header>
-             
-            </body>
-        </html>
-   
-        <html lang="en">
-            <body>
-                <header>
-                    <nav>
-                        <Link href="/">Home</Link> | <Link href="/rota1">Rota 1</Link> | <Link href="/rota3">Rota 3</Link>
-                    </nav>
-                </header>
-             
-            </body>
-        </html>
-  
-        <html lang="en">
-            <body>
-                <header>
-                    <nav>
-                        <Link href="/">Home</Link> | <Link href="/rota1">Rota 1</Link> | <Link href="/rota2">Rota 2</Link>
-                    </nav>
-                </header>
-               
-            </body>
-        </html>
-  
+
+
+export default function Home(){
+
+    return (
+
+        <div>           
+
+            <div>
+
+                <h1>
+
+                    Viva Santana!
+
+                </h1>
+
+                <Link href="/novarota">Rota1</Link> <br/><br/>
+
+                <a href="/novarota">Rota 1, jeito antigo</a>
+
+            </div>
+
+        </div>       
+
+    )
+
+}
