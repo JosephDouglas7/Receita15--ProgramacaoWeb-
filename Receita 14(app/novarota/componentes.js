a9c45b6c0a1e614450b35a4539b80ab30dd4df75
JosephDouglas7 <142358684+JosephDouglas7@users.noreply.github.com>
2025-07-16 10:11:33 -0300

    Create componentes.js

---
+++ Receita 14(app/novarota/componentes.js
@@ -0,0 +1,8 @@
+export function MariaPrea() {
+    return (
+        <div>
+            <h2>"O sertão vai virar mar, o mar vai virar sertão!" </h2>
+            <h3>"Morreu Maria Preá..."</h3>   
+        </div>
+    );
+}
